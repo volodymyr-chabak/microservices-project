840623f7d68a07e3b55276b22c9a6a7d6df27027
volodymyr-chabak <volodya.chabak@gmail.com>
2021-01-22 22:09:07 +0200

    Initial commit.

---
+++ worker-service/src/main/java/ua/chabak/workerservice/WorkerServiceApplication.java
@@ -0,0 +1,15 @@
+package ua.chabak.workerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+
+@SpringBootApplication
+public class WorkerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerServiceApplication.class, args);
+    }
+
+}
---
+++ worker-service/src/test/java/ua/chabak/workerservice/WorkerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.chabak.workerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
